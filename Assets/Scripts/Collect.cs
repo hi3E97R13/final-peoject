5bd45994987db2f99e8afb9ef10dbf06a7e56e4f
Reynolds_Elliot <reyne251@hsd.k12.or.us>
2024-01-29 13:56:18 -0800

    sound day 4

--- Assets/Scripts/Collect.cs
+++ Assets/Scripts/Collect.cs
@@ -8,6 +8,7 @@ public class Collect : MonoBehaviour
 {
     public AudioClip collect;
     AudioSource audioSource;
+    public GameObject gameObject;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +19,6 @@ public class Collect : MonoBehaviour
     void OnCollisionEnter(Collision collision){
         audioSource.PlayOneShot(collect, 0.5F);
         Destroy(gameObject, 0.4F);
+        Debug.Log("work dammit");
     }   
 }
